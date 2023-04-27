29924fccfb337b2eb4ede58814bd157c3c5d4b9b
valeriofilareti <valerio.filareti@gmail.com>
2023-04-27 16:09:10 +0200

    [add] header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 
 <script>
+import Header from './components/Header.vue'
 export default{
-  
+  name: 'App',
+  components: {
+    Header
+  },
+  data(){
+    return {
+      title: 'dc-vite'
+    }
+  }
 }
 </script>
 <template>
+  <Header />
 
 </template>
 
