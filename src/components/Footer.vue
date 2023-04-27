c8b5be0111e9dd244f04ed7a9fb1166b89639467
valeriofilareti <valerio.filareti@gmail.com>
2023-04-27 18:48:26 +0200

    [add] footer structure

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,16 +7,89 @@ export default {
 
 <template>
   <footer>
-    <div class="footer-top"></div>
+    <div class="container">
+      <div class="footer-top d-flex justify-between">
+        <div class="footer-links d-flex">
+          <div class="col">
+            <ul>
+              <li><h2>link</h2></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+            </ul>
+          </div>
+          <div class="col">
+            <ul>
+              <li><h2>link</h2></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+            </ul>
+          </div>
+          <div class="col">
+            <ul>
+              <li><h2>link</h2></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+              <li><a href="#">link</a></li>
+            </ul>
+          </div>
+          <div class="col"></div>
+          <div class="col"></div>
+        </div>
+        <div class="footer-img">
+          <img src="../assets/img/dc-logo-bg.png" alt="">
+        </div>
+      </div>
+    </div>
     <div class="footer-bottom"></div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
   @use '../scss/main.scss';
-  .footer-top {
-    height: 250px;
-    background-image: url(../assets/img/footer-bg.jpg);
 
+  footer {
+    background-image: url(../assets/img/footer-bg.jpg);
+  }
+  .footer-top {
+    height: 350px;
+
+    .col {
+      padding: 10px;
+      li {
+        margin: 5px 0;
+      }
+      
+    }
+    a {
+      color: white;
+    }
+  }
+  .footer-bottom {
+    height: 80px;
+    background-color: gray;
+  }
+
+  .footer-img {
+    width: 500px;
+    overflow: hidden;
   }
 </style>
